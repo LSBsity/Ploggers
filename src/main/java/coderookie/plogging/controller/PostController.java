b23974881ffde98b6255487605d5598fd258093f
LSBsity <dltmdqls51@naver.com>
2024-05-29 19:15:39 +0900

    test

--- src/main/java/coderookie/plogging/controller/PostController.java
+++ src/main/java/coderookie/plogging/controller/PostController.java
@@ -6,12 +6,14 @@ import coderookie.plogging.dto.request.post.PostRequestDto;
 import coderookie.plogging.dto.response.post.*;
 import coderookie.plogging.search.SearchCond;
 import coderookie.plogging.service.PostService;
+import io.swagger.annotations.ApiOperation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +22,10 @@ public class PostController {
 
     private final PostService postService;
 
+
+    @ApiOperation(
+            value = "게시물 상세 보기"
+            , notes = "path로 주어진 postId를 통해 게시물을 상세 조회")
     @GetMapping("/{postId}")
     public ResponseEntity<? super GetPostResponseDto> getPost(
 
@@ -28,6 +34,9 @@ public class PostController {
         return postService.getPost(postId);
     }
 
+    @ApiOperation(
+            value = "게시물 조회수 1 올리기"
+            , notes = "path로 주어진 postId를 통해 게시물의 조회수를 올림")
     @PatchMapping("/{postId}/viewCount")
     public ResponseEntity<? super IncreaseViewCountResponseDto> increaseViewCount(
 
@@ -36,6 +45,9 @@ public class PostController {
         return postService.increaseViewCount(postId);
     }
 
+    @ApiOperation(
+            value = "좋아요를 누른 사용자 정보 가져오기"
+            , notes = "path로 주어진 postId를 이용해 게시물에 좋아요를 누른 사용자들의 정보를 가져옴")
     @GetMapping("/{postId}/likes")
     public ResponseEntity<? super GetLikersDto> getLikesList(
 
@@ -44,6 +56,9 @@ public class PostController {
         return postService.getLikersList(postId);
     }
 
+    @ApiOperation(
+            value = "게시물의 댓글 가져오기"
+            , notes = "path로 주어진 postId를 통해 모든 댓글을 가져옴")
     @GetMapping("/{postId}/comments")
     public ResponseEntity<? super GetCommentListResponseDto> getCommentList(
 
@@ -52,6 +67,9 @@ public class PostController {
         return postService.getCommentList(postId);
     }
 
+    @ApiOperation(
+            value = "베스트 게시물 가져오기"
+            , notes = "좋아요가 가장 많이 달린 3개의 게시물을 가져옴")
     @GetMapping("/bestPosts")
     public ResponseEntity<? super GetBestPostsResponseDto> getBestPosts(
 
@@ -59,6 +77,9 @@ public class PostController {
         return postService.getBests();
     }
 
+    @ApiOperation(
+            value = "사용자가 작성한 게시물 가져오기"
+            , notes = "path로 주어진 email을 통해 사용자의 모든 게시물을 가져옴")
     @GetMapping("/{email}/posts")
     public ResponseEntity<? super GetUserPostListResponseDto> getUserPosts(
 
@@ -67,6 +88,9 @@ public class PostController {
         return postService.getUserPostList(email);
     }
 
+    @ApiOperation(
+            value = "메인 화면에서 게시물 가져오기"
+            , notes = "메인 화면에서 보여질 게시물들을 페이징 처리 or 검색을 이용하여 가져옴")
     @GetMapping("")
     public ResponseEntity<? super GetPostMainResponseDto> posts(
             @RequestBody(required = false) SearchCond searchCond,
@@ -75,6 +99,9 @@ public class PostController {
         return postService.getMainPosts(searchCond, pageable);
     }
 
+    @ApiOperation(
+            value = "게시물 작성하기"
+            , notes = "게시물 작성 화면에서 사용자가 작성한 게시물을 게시함")
     @PostMapping("")
     public ResponseEntity<? super PostResponseDto> posting(
 
@@ -84,6 +111,9 @@ public class PostController {
         return postService.posting(request, email);
     }
 
+    @ApiOperation(
+            value = "게시물 댓글 작성하기"
+            , notes = "path로 주어진 postId를 가진 게시물의 상세 화면에서 사용자가 댓글을 게시함")
     @PostMapping("/{postId}/comments")
     public ResponseEntity<? super PostCommentResponseDto> comment(
 
@@ -95,6 +125,9 @@ public class PostController {
         return postService.postComment(request, postId, email);
     }
 
+    @ApiOperation(
+            value = "게시물 좋아요 누르기"
+            , notes = "path로 주어진 postId를 가진 게시물의 상세 화면에서 사용자가 좋아요를 누름")
     @PutMapping("/{postId}/likes")
     public ResponseEntity<? super PutPostLikesResponseDto> putLikes(
 
@@ -104,6 +137,9 @@ public class PostController {
         return postService.putLikes(postId, email);
     }
 
+    @ApiOperation(
+            value = "게시물 수정하기"
+            , notes = "path로 주어진 postId를 가진 게시물의 수정 화면에서 사용자가 수정함")
     @PatchMapping("/{postId}")
     public ResponseEntity<? super EditPostResponseDto> editPost(
 
@@ -114,6 +150,9 @@ public class PostController {
         return postService.editPost(request, postId, email);
     }
 
+    @ApiOperation(
+            value = "게시물 삭제하기"
+            , notes = "path로 주어진 postId를 가진 게시물의 상세 화면에서 사용자가 삭제함")
     @DeleteMapping("/{postId}")
     public ResponseEntity<? super DeletePostResponseDto> deletePost(
 
